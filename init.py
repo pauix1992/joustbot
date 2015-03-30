8fdceb00414566270f93a82b603da5e92034bc1d
Pau Amich Vidal <pamich1@gmail.com>
2015-03-30 17:49:01 +0200

    Uncommented everything

--- init.py
+++ init.py
@@ -6,11 +6,11 @@ from random import randint;
 
 
 # import custom classes
-#from joust import *
-#from melee import *
+from joust import *
+from melee import *
 from archery import *
-#from horse_racing import *
-#from duel import *
+from horse_racing import *
+from duel import *
 
 # Array that stores the IDs of checked comments
 checked_comments = []
@@ -26,10 +26,10 @@ def start_bot():
         checked_comments.append(comment.id)
 
     while(1):
- #       try:
-        find_new_comments(subreddits)
-#        except Exception:
-        #print("SOMETHING FUCKED UP!")
+        try:
+            find_new_comments(subreddits)
+        except Exception:
+        print("SOMETHING FUCKED UP!")
         sleep(60)
 
 
@@ -40,55 +40,55 @@ def find_new_comments(subreddits):
     for comment in comments:
         body = comment.body
         if body.find("joustbot") >= 0 and comment.id not in checked_comments:
-       #     
-            if body.find("joustbot joust") >= 0:
+           
+            if body.find(" joust") >= 0:
                 print("rolling a joust")
-       #         b = body.split("\n")
-       #         result = ''
-       #        for pair in b:
-       #            c = pair.split(" - ")
-       #            if len(c) > 1:
-       #                result +="***" + c[0] + " VERSUS " + c[1] + "!***\n\n"
-       #                result += joust(c[0],c[1]) or "ERROR!"
-       #                result += "------------------------------------------------------\n\n"
-       #        comment.reply(result[:9999])
-       #        if len(result) > 10000:
-       #            comment.reply(result[10000:19999])
-       #        if len(result) > 20000:
-       #            comment.reply(result[20000:29999])
-       #
-       #
-            elif body.find("joustbot melee") >= 0:
+                b = body.split("\n")
+                result = ''
+               for pair in b:
+                   c = pair.split(" - ")
+                   if len(c) > 1:
+                       result +="***" + c[0] + " VERSUS " + c[1] + "!***\n\n"
+                       result += joust(c[0],c[1]) or "ERROR!"
+                       result += "------------------------------------------------------\n\n"
+               comment.reply(result[:9999])
+               if len(result) > 10000:
+                   comment.reply(result[10000:19999])
+               if len(result) > 20000:
+                   comment.reply(result[20000:29999])
+       
+       
+            elif body.find(" melee") >= 0:
                 print("rolling a melee")
-       #        b = body.split("\n")
-       #        c = []
-       #        for contestant in b:
-       #            if contestant.find("joustbot") < 0:
-       #                c.append(contestant)
-       #        result = melee(*c)
-       #        comment.reply(result[:9999])
-       #        if len(result) > 10000:
-       #            comment.reply(result[10000:19999])
-       #        if len(result) > 20000:
-       #            comment.reply(result[20000:29999])             
-       #            print(result)
-       #
-       #
-            elif body.find("josutbot horse race") >= 0:
+               b = body.split("\n")
+               c = []
+               for contestant in b:
+                   if contestant.find("joustbot") < 0:
+                       c.append(contestant)
+               result = melee(*c)
+               comment.reply(result[:9999])
+               if len(result) > 10000:
+                   comment.reply(result[10000:19999])
+               if len(result) > 20000:
+                   comment.reply(result[20000:29999])             
+                   print(result)
+       
+       
+            elif body.find(" horse race") >= 0:
                 print("rolling a horse race")
-       #        b = body.split("\n")
-       #        c = []
-       #        for contestant in b:
-       #            if contestant.find("joustbot") < 0:
-       #                c.append(contestant)
-       #        result = horse_racing(*c)
-       #        comment.reply(result[:9999])
-       #        if len(result) > 10000:
-       #            comment.reply(result[10000:19999])
-       #        if len(result) > 20000:
-       #            comment.reply(result[20000:29999])
-       #
-       #
+               b = body.split("\n")
+               c = []
+               for contestant in b:
+                   if contestant.find("joustbot") < 0:
+                       c.append(contestant)
+               result = horse_racing(*c)
+               comment.reply(result[:9999])
+               if len(result) > 10000:
+                   comment.reply(result[10000:19999])
+               if len(result) > 20000:
+                   comment.reply(result[20000:29999])
+       
+       
             elif body.find(" archery") >= 0:
                 print("rolling an archery competition")
                 b = body.split("\n")
@@ -105,18 +105,18 @@ def find_new_comments(subreddits):
 
             elif body.find("joustbot duel") >= 0:
                 print("rolling a duel")
-       #        b = body.split("\n")
-       #        result = ''
-       #        for pair in b:
-       #            c = pair.split(" - ")
-       #            if len(c) > 1:
-       #                result +="***" + c[0] + " VERSUS " + c[1] + "!***\n\n"
-       #                result += duel(c[0],c[1]) or "ERROR!"
-       #                result += "------------------------------------------------------\n\n"
-       #        comment.reply(result[:9999])
-       #        if len(result) > 10000:
-       #            comment.reply(result[10000:19999])
-       #        if len(result) > 20000:
-       #            comment.reply(result[20000:29999])
+               b = body.split("\n")
+               result = ''
+               for pair in b:
+                   c = pair.split(" - ")
+                   if len(c) > 1:
+                       result +="***" + c[0] + " VERSUS " + c[1] + "!***\n\n"
+                       result += duel(c[0],c[1]) or "ERROR!"
+                       result += "------------------------------------------------------\n\n"
+               comment.reply(result[:9999])
+               if len(result) > 10000:
+                   comment.reply(result[10000:19999])
+               if len(result) > 20000:
+                   comment.reply(result[20000:29999])
 
             checked_comments.append(comment.id)
